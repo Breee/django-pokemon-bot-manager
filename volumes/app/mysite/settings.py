bdfa106dd9d1a77d0a081a670faadc4a8c095999
Lukas Veneziano <fs@venezilu.de>
2018-03-10 12:20:56 +0100

    fix postgres standard password not consistant with docker compose dist file

--- volumes/app/mysite/settings.py
+++ volumes/app/mysite/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.environ.get('DB_ENV_DB', 'postgres'),
         'USER': os.environ.get('DB_ENV_POSTGRES_USER', 'postgres'),
-        'PASSWORD': os.environ.get('DB_ENV_POSTGRES_PASSWORD', 'postgres'),
+        'PASSWORD': os.environ.get('DB_ENV_POSTGRES_PASSWORD', 'changeme'),
         'HOST': os.environ.get('DB_PORT_5432_TCP_ADDR', 'db'),
         'PORT': os.environ.get('DB_PORT_5432_TCP_PORT', ''),
     },
