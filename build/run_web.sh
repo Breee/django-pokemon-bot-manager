2fe922c0cfa9dcd03386297ae6b12b8dfd07e91e
Lukas Veneziano <fs@venezilu.de>
2018-03-12 22:56:45 +0100

    added nginx proxy in front of django and changed it to use uwsgi

--- build/run_web.sh
+++ build/run_web.sh
@@ -8,5 +8,10 @@ cd /app
 python manage.py makemigrations
 # migrate db, so we have the latest db schema
 python manage.py migrate
+# collect static files
+python manage.py collectstatic --no-input
 # start development server on public ip interface, on port 8000
-python manage.py runserver 0.0.0.0:8000
+uwsgi --socket :8000 --module mysite.wsgi
+
+## insecure way to run the server
+# python manage.py runserver 0.0.0.0:8000
