1a0b1d29dd5976546fdb22555bef3c5527922247
Lukas Veneziano <fs@venezilu.de>
2018-03-15 14:37:06 +0100

    add javascript for map

--- volumes/app/myapp/views.py
+++ volumes/app/myapp/views.py
@@ -1,38 +1,15 @@
-from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
-import csv
+from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
 
-from .models import Pokemon
-from .serializers import PokemonSerializer
 
-
-@csrf_exempt
-def test(request):
-    """
-    Load Pokedex if Database is empty
-    Todo: Write real init!
-    """
-    if Pokemon.objects.count() == 0:
-        with open('pokedex.csv') as pokedex:
-            reader = csv.DictReader(pokedex)
-            for poke in reader:
-                newpoke = Pokemon(poke_nr=poke['species_id'], poke_name_eng=poke['identifier'])
-                newpoke.save()
-            pokedex.close()
-        with open('pokedex_ger.csv') as pokedex_ger:
-            reader = csv.DictReader(pokedex_ger)
-            for poke in reader:
-                pid = poke['Ndex']
-                newpoke = Pokemon(poke_nr=int(pid), poke_name_ger=poke['German'])
-                newpoke.save()
-            pokedex.close()
-    """
-    List all Pokemon with JSON
-    """
-    if request.method == 'GET':
-        poke = Pokemon.objects.all()
-        serializer = PokemonSerializer(poke, many=True)
-        return JsonResponse(serializer.data, safe=False)
+@login_required
+def pokemap(request):
+    context = {
+    }
+    return render(request, 'map/index.html', context)
 
 
 def up(request):
---
+++ volumes/app/static_dirs/js/myleaflet.js
@@ -0,0 +1,20 @@
+var mymap = L.map('mapid').setView([48.0, 7.8], 13);
+  L.tileLayer('https://tiles.venezilu.de/styles/osm-bright/{z}/{x}/{y}.png', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        maxZoom: 18,
+        id: 'openstreetmap',
+  }).addTo(mymap);
+  $.getJSON("http://localhost:8000/api/pokepositions2", function(data) {
+    for (var i in data) {
+        var pokemon = data[i];
+        var popup = "" + pokemon.poke_despawn_time + "<br>" +
+                    "" + pokemon.poke_nr;
+        L.marker([pokemon.poke_lat, pokemon.poke_lon],
+            {title: "test",
+             icon: L.icon({
+                 iconUrl: "/static/img/pokemons/" + pokemon.poke_nr + '.png',
+                 iconSize: [32, 32],
+                 popupAnchor: [-3, -76]
+             }) }).addTo(mymap).bindPopup(popup);
+    }
+  });
