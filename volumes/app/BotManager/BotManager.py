e979a35b9490d4476a76842b4554ad6bc0af066b
Lukas Veneziano <fs@venezilu.de>
2018-03-16 02:21:46 +0100

    add new BotManager

--- volumes/app/BotManager/BotManager.py
+++ volumes/app/BotManager/BotManager.py
@@ -1,76 +1,84 @@
+from mysite.settings import *
+import pexpect
 from collections import namedtuple
-from multiprocessing import Process
-import importlib.util
-import os
-import sys
 
+BotStruct = namedtuple("BotStruct", "path main_file name")
 
 
 class BotManager:
-    '''
+    """
         struct for defining bots
         name: name of the bots class
         config: configs for the bot
         path: path to the bots files
         main_file: main python file of the bot e.g. 'Bot.py'
-    '''
-    BotStruct = namedtuple("BotStruct", "name config path main_file")
-
+    """
     # define bots
-    bots = [BotStruct(name="PollBot",
-                      config={'prefix': '!raid', 'description': 'Raidquaza',
-                              'config_file': '/home/lukas/src/pokemon-raid-bot/config.conf'},
-                      path="/home/lukas/src/pokemon-raid-bot",
-                      main_file='pollbot/PollBot.py')]
+    bots = BOTS
 
     def __init__(self):
         self.bot_instances = []
-        self.processes = []
-        self.pid = 0
+        self.bot_outputs = []
         for bot_struct in BotManager.bots:
-            if not bot_struct.path.startswith('/'):
-                path = '/' + os.path.relpath(os.getcwd() + '/../' + bot_struct.path, '/')
-            else:
-                path = bot_struct.path
+            self.bot_instances.append(None)
+            self.bot_outputs.append('')
 
-            # add bot path to sys path to make usage of submodules possible
-            sys.path.append(path)
-
-            spec = importlib.util.spec_from_file_location(bot_struct.name,
-                                                          path + '/' + bot_struct.main_file)
-            module = importlib.util.module_from_spec(spec)
-            spec.loader.exec_module(module)
-            instance = getattr(module,
-                               bot_struct.name)(bot_struct.config['prefix'],
-                                                bot_struct.config['description'],
-                                                bot_struct.config['config_file'])
-            self.bot_instances.append(instance)
-
-            self.processes.append(None)
-
-    def get_bot(self, index):
-        if index not in range(0, len(BotManager.bots), 1):
-            return None
-        return self.bot_instances[index]
+    def _bot_exists(self, index):
+        if index not in range(0, len(BotManager.bots), 1) or self.bot_instances[index] is None:
+            return False
+        else:
+            return True
 
     def run_bot(self, index):
         if index not in range(0, len(BotManager.bots), 1):
-            return None
+            return False
 
-        def bot_runner(bot, pid):
-            import os
-            pid = os.getpid()
-            print(pid)
-            bot.run()
-
-        bot = self.bot_instances[index]
-        self.processes[index] = Process(target=bot_runner, args=[bot, self.pid])
-        self.processes[index].start()
-        print(self.pid)
+        bot = self.bots[index]
+        self.bot_instances[index] = pexpect.spawn('python3', [bot.main_file], cwd=bot.path,
+                                                  encoding='utf-8')
+        return True
 
     def close_bot(self, index):
-        if index not in range(0, len(BotManager.bots), 1):
-            return
-        self.processes[index].terminate()
-        print(self.pid)
-        self.processes[index].join()
+        if not self._bot_exists(index):
+            return False
+
+        bot = self.bot_instances[index]
+
+        bot.sendcontrol('c')
+        return True
+
+    def kill_bot(self, index):
+        if not self._bot_exists(index):
+            return False
+
+        bot = self.bot_instances[index]
+        bot.sendcontrol('c')
+        return True
+
+    def get_bot_output(self, index):
+        if not self._bot_exists(index):
+            return False
+
+        bot = self.bot_instances[index]
+        # read as long as there is new input
+        while True:
+            try:
+                self.bot_outputs[index] += bot.read_nonblocking(100, timeout=0)
+            except pexpect.TIMEOUT:
+                break
+
+        return self.bot_outputs[index]
+
+    def get_bot_status(self, index):
+        if not self._bot_exists(index):
+            return False
+
+        bot = self.bot_instances[index]
+        return bot.isalive()
+
+    def get_bot_list(self):
+        bot_list = []
+        for bot in self.bots:
+            bot_list.append(bot.name)
+        return bot_list
+
