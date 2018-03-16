a5b805abe11e5b3ac11fe49666ac888fb4635b9d
Lukas Veneziano <fs@venezilu.de>
2018-03-16 02:21:19 +0100

    deleted unused code

--- volumes/app/myapp/tasks.py
+++ volumes/app/myapp/tasks.py
@@ -1,13 +1,8 @@
 # Create your tasks here
-from __future__ import absolute_import, unicode_literals
-from celery import shared_task
 from celery.contrib.abortable import AbortableTask
+from mysite.celery import capp
 
-bot_processes = []
 
-
-@shared_task(bind=True, base=AbortableTask)
+@capp.task(bind=True, base=AbortableTask)
 def cel_bot_start(self):
-    while not self.is_aborted():
-        print('I am running')
-    print('I was aborted!')
+    pass
