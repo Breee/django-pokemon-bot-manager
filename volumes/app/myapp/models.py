d8410d5f2d110a90be8900bb70f79f1f4bc1f0ad
Lukas Veneziano <fs@venezilu.de>
2018-03-20 12:49:10 +0100

    removed redundant id field

--- volumes/app/myapp/models.py
+++ volumes/app/myapp/models.py
@@ -18,7 +18,6 @@ class Pokemon(models.Model):
 
 class PokePosition(models.Model):
     poke_report_time = models.DateTimeField(default=timezone.now)
-    poke_pos_id = models.IntegerField(primary_key=True, auto_created=True)
     poke_lvl = models.IntegerField(default=0)
     poke_iv = models.FloatField(default=.0)
     poke_lat = models.FloatField()
