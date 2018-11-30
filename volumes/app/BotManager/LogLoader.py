ec6acd59e64d2a89695d43b691f66dbd28b2835f
Trigrab <trigrab@gmail.com>
2018-11-30 15:51:35 +0100

    fix: dirty hack to be more robust

--- volumes/app/BotManager/LogLoader.py
+++ volumes/app/BotManager/LogLoader.py
@@ -7,9 +7,20 @@ if __name__ == "__main__":
     try:
         while True:
             for i in range(0, int(sys.argv[1]), 1):
-                r = requests.get('http://nginx/bot/status?bot=' + str(i))
-                if r.status_code == 200:
-                    r = requests.get('http://nginx/bot/output/pull?bot=' + str(i))
+                r = None
+                try:
+                    r = requests.get('http://nginx/bot/status?bot=' + str(i))
+                except requests.exceptions.Timeout:
+                    pass
+                except requests.exceptions.TooManyRedirects:
+                    pass
+                except requests.exceptions.RequestException as e:
+                    # catastrophic error. bail.
+                    print(e)
+
+                if r is not None:
+                    if r.status_code == 200:
+                        r = requests.get('http://nginx/bot/output/pull?bot=' + str(i))
             time.sleep(1)
     except KeyboardInterrupt:
         print('interrupted!')
