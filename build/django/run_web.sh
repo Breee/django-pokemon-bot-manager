517d4a976652a3e5c59e2e7a0e41fcad3878a140
Trigrab <trigrab@gmail.com>
2018-12-05 18:33:39 +0100

    fix: boot

--- build/django/run_web.sh
+++ build/django/run_web.sh
@@ -27,7 +27,10 @@ python manage.py migrate
 # collect static files
 python manage.py collectstatic --no-input
 # start development server on public ip interface, on port 8000
-su - django -c "cd /app && uwsgi --socket :8000 --module mysite.wsgi"
+#su - django -c "cd /app && uwsgi --socket :8000 --module mysite.wsgi"
+
+# start deployment server on port 8000
+daphne -b 0.0.0.0 -p 8000 mysite.asgi:application
 
 ## insecure way to run the server
 # python manage.py runserver 0.0.0.0:8000
