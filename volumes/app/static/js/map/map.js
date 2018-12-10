92bd7c2590a50c983d42cb585b4e5d69f497c917
Trigrab <trigrab@gmail.com>
2018-12-10 14:01:31 +0100

    improve: build more robust map with cookies and better structure

---
+++ volumes/app/static/js/map/map.js
@@ -0,0 +1,134 @@
+"use strict";
+
+function MapCookie() {
+    this.pokestopsHidden = getBooleanCookieValue('pokestopsHidden', true);
+    this.gymsHidden = getBooleanCookieValue('gymsHidden', false);
+    this.regularPokemonHidden = getBooleanCookieValue('regularPokemonHidden', false);
+    this.ivPokemonHidden = getBooleanCookieValue('ivPokemonHidden', false);
+    for (var key in this) {
+            if (getBooleanCookieValue(key) === undefined) {
+                setCookie(key, this[key]);
+            }
+    }
+}
+
+MapCookie.prototype.toggleCookieSetting = function (name) {
+    this[name] = !this[name];
+    setCookie(name, this[name], 365);
+    return this[name]
+};
+
+function setCheckboxes () {
+    console.log(mapCookie)
+    var checkboxes = $( "input:checkbox" );
+    for (var checkbox in checkboxes) {
+        if (checkboxes.hasOwnProperty(checkbox)) {
+            var cb = checkboxes[checkbox];
+            if (cb.tagName === 'INPUT') {
+                var cb_bool = mapCookie[cb.name];
+                $(cb).attr( {checked: (!cb_bool)});
+                $(cb).prop( {checked: (!cb_bool)});
+            }
+        }
+    }
+
+}
+
+var mapCookie = new MapCookie();
+setCheckboxes();
+
+var csrftoken = getCookie('csrftoken');
+$.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+    }
+});
+
+var pokedex = undefined;
+$.getJSON('/api/pokedex/', function (data) {
+    pokedex = data;
+});
+
+var getPokemonData = function () {
+    $.getJSON("/api/pokemon/spawns", function(data) {
+        addPokemonToMap(data)
+    });
+};
+
+var getPointOfInterestData = function() {
+    $.getJSON("/api/poi/all", function (data) {
+        addPointOfInterestToMap(data);
+    });
+};
+
+var togglePokestops = function() {
+    var pokestopsHidden = mapCookie.toggleCookieSetting('pokestopsHidden');
+    toggleMapLayer(pokestopLayer, pokestopsHidden);
+};
+
+var toggleGyms = function() {
+    var gymsHidden = mapCookie.toggleCookieSetting('gymsHidden');
+    toggleMapLayer(gymLayer, gymsHidden);
+};
+
+var toggleIVPokemon = function() {
+    var ivPokemonHidden = mapCookie.toggleCookieSetting('ivPokemonHidden');
+    toggleMapLayer(ivpokemonGroup, ivPokemonHidden);
+};
+
+var toggleRegularPokemon = function() {
+    var regularPokemonHidden = mapCookie.toggleCookieSetting('regularPokemonHidden');
+    toggleMapLayer(pokemonGroup, regularPokemonHidden);
+};
+
+
+var update_time = function() {
+    var date = new Date();
+    return date.getTime();
+};
+
+var last_update = 0;
+var loading_data = false;
+
+function reloadData() {
+    // if pokestop not yet loaded or another pokemon update is not long ago, wait a sec
+
+        if (!loading_data) {
+            if (pokedex === undefined) {
+                setTimeout(function () {
+                    reloadData()
+                }, 200);
+                return
+            }
+            loading_data = true;
+            last_update = update_time();
+            getPointOfInterestData();
+            getPokemonData();
+            setTimeout(function () {
+                loading_data = false;
+            }, 2000);
+        }
+}
+
+var websocket_protocol = 'ws://';
+if (location.protocol === 'https:') {
+    websocket_protocol = 'wss://'
+}
+
+var updateSocket = new ReconnectingWebSocket(
+websocket_protocol + window.location.host +
+'/ws/update/');
+
+updateSocket.onmessage = function(e) {
+    var data = JSON.parse(e.data);
+    console.log(data);
+    if (data.type === 'change') {
+        reloadData()
+    }
+};
+
+reloadData();
+
+$( function() {
+    $( "#datepicker" ).datepicker();
+} );
---
+++ volumes/app/static/js/map/myleaflet.js
@@ -0,0 +1,201 @@
+"use strict";
+
+function clusterIcon(cluster) {
+		var childCount = cluster.getChildCount();
+
+		var c = ' marker-cluster-';
+		if (childCount < 10) {
+			c += 'small';
+		} else if (childCount < 100) {
+			c += 'medium';
+		} else {
+			c += 'large';
+		}
+
+		return new L.DivIcon({
+            html: '<div style="opacity: 0.5;" ><span>' + childCount + '</span></div>',
+            className: 'marker-cluster' + c,
+            iconSize: new L.Point(40, 40)
+		});
+}
+
+
+
+
+
+var mymap = L.map('map').setView([47.9960526,7.8464833], 13);
+var ivpokemonGroup = undefined;
+var pokemonGroup = undefined;
+var pokestopLayer = undefined;
+var gymLayer = undefined;
+var url = 'https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png';
+
+L.tileLayer(url, {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'openstreetmap'
+}).addTo(mymap);
+
+var sidebar = L.control.sidebar('sidebar').addTo(mymap);
+
+var get_popup_data = function(pokemon) {
+    var date = new Date(pokemon.disappear_time);
+
+    var today = new Date();
+    var date_str = '';
+    if (date.getDate() > today.getDate()) {
+        date_str = date.toLocaleDateString('de-DE') + ' ';
+    }
+    if (pokemon.individual_stamina != null) {
+        var iv_sta = pokemon.individual_stamina;
+        var iv_att = pokemon.individual_attack;
+        var iv_def = pokemon.individual_defense;
+        var iv_proc = Math.round(((iv_sta + iv_att + iv_def)/45)*10000)/100;
+        var iv_str = '<b>IV:</b>' + iv_proc + '% ' + '(A' + iv_att + '|D' + iv_def + '|S' + iv_sta + ')<br>';
+        var cp_str = '<b>CP:</b>' + pokemon.cp + '<br>';
+    }
+    else {
+        iv_str = '';
+        cp_str = '';
+    }
+
+    var maps_str = '<a href="https://www.google.com/maps/place/' + pokemon.latitude + ',' +
+        pokemon.longitude + '" target="_blank" title="Open in Google Maps">' + 'Maps</a><br>';
+
+    return '<h3>' + pokedex[pokemon.pokemon_object -1].name_german + ' (' + pokemon.pokemon_object + ')' + '</h3>' +
+        '<b>Despawn Time</b> ' + date_str + date.toLocaleTimeString('de-DE') + '<br>' +
+         iv_str + cp_str + maps_str;
+};
+
+function addPokemonToMap(data) {
+        if (pokemonGroup !== undefined && ivpokemonGroup !== undefined) {
+            pokemonGroup.clearLayers();
+            ivpokemonGroup.clearLayers();
+        }
+        else {
+            pokemonGroup = L.layerGroup();
+            ivpokemonGroup = L.layerGroup();
+        }
+        for (var i in data) {
+            if (data.hasOwnProperty(i)) {
+                var pokemon = data[i];
+
+                var popup = get_popup_data(pokemon);
+                var marker = L.marker([pokemon.latitude, pokemon.longitude],
+                    {
+                        title: pokedex[pokemon.pokemon_object - 1].name_german,
+                        icon: L.icon({
+                            iconUrl: "/static/img/pokemons/" + pokemon.pokemon_object + '.png',
+                            iconSize: [32, 32],
+                            popupAnchor: [-3, -76]
+                        })
+                    });
+                marker.bindPopup(popup);
+                if (pokemon.individual_stamina !== null || pokemon.individual_attack !== null || pokemon.individual_defense !== null) {
+                    ivpokemonGroup.addLayer(marker);
+                } else {
+                    pokemonGroup.addLayer(marker);
+                }
+            }
+        }
+        if (!mapCookie.ivPokemonHidden) {
+            ivpokemonGroup.addTo(mymap);
+        }
+        if (!mapCookie.regularPokemonHidden) {
+            pokemonGroup.addTo(mymap);
+        }
+}
+
+
+function addPointOfInterestToMap(data) {
+        if (pokestopLayer !== undefined) {
+            gymLayer.clearLayers();
+            pokestopLayer.clearLayers();
+        } else {
+            gymLayer = L.markerClusterGroup({
+                maxClusterRadius: 120,
+                disableClusteringAtZoom: 15,
+                iconCreateFunction: clusterIcon
+            });
+            pokestopLayer = L.markerClusterGroup({
+                maxClusterRadius: 120,
+                disableClusteringAtZoom: 17,
+                iconCreateFunction: clusterIcon
+            });
+        }
+        for (var i in data) {
+            if (data.hasOwnProperty(i)) {
+
+                var poi = data[i];
+                var popup = "" + poi.name + "<br>";
+                var marker;
+                if (poi.image_url !== null) {
+                    popup += '<img style="width:125px; height: 125px; object-fit: cover;" src="' + poi.image_url + '" />'
+                }
+                if (poi.type === "pokestop") {
+                    marker = L.marker([poi.latitude, poi.longitude],
+                        {
+                            title: poi.name,
+                            icon: L.icon({
+                                iconUrl: "/static/img/map/pstop.png",
+                                iconSize: [32, 32],
+                                popupAnchor: [-3, -76]
+                            })
+                        });
+                    marker.bindPopup(popup);
+                    pokestopLayer.addLayer(marker);
+                } else if (poi.type === "gym") {
+                    var iconUrl = "/static/img/map/gym.png";
+                    if (poi.park === true) {
+                        iconUrl = "/static/img/map/ex_gym.png"
+                    }
+                    marker = L.marker([poi.latitude, poi.longitude],
+                        {
+                            title: poi.name,
+                            icon: L.icon({
+                                iconUrl: iconUrl,
+                                iconSize: [32, 32],
+                                popupAnchor: [-3, -76]
+                            }),
+                            opacity: 0.8
+                        });
+                    marker.bindPopup(popup);
+                    gymLayer.addLayer(marker);
+                } else {
+                    marker = L.marker([poi.latitude, poi.longitude],
+                        {
+                            title: poi.name,
+                            icon: L.icon({
+                                iconUrl: "/static/img/map/pstop.png",
+                                iconSize: [32, 32],
+                                popupAnchor: [-3, -76]
+                            })
+                        });
+                    marker.bindPopup(popup);
+                    pokestopLayer.addLayer(marker);
+                }
+            }
+        }
+        if (!mapCookie.gymsHidden) {
+            gymLayer.addTo(mymap);
+        }
+        if (!mapCookie.pokestopsHidden) {
+            pokestopLayer.addTo(mymap);
+        }
+}
+
+
+
+var toggleMapLayer = function(layer, bool) {
+    console.log(layer);
+    console.log(bool);
+     if (!bool) {
+        layer.addTo(mymap);
+    }
+    else {
+        layer.remove(mymap);
+    }
+    return !bool;
+};
+
+
