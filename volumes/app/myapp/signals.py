38c42af63f4b95624a95732be94a369c412bc530
Trigrab <trigrab@gmail.com>
2018-12-11 18:35:54 +0100

    fix: too many updates; only update every 5s on websocket

--- volumes/app/myapp/signals.py
+++ volumes/app/myapp/signals.py
@@ -1,31 +1,34 @@
+from datetime import timedelta
+
 import channels.layers
 from asgiref.sync import async_to_sync
+from django.db.models import Model
 from django.db.models.signals import post_save
+from django.utils import timezone
 
 from myapp.models import PokemonSpawn, PointOfInterest, Mapper
 
+last_updated = timezone.now()
+
 
 def send_pokemon_update_to_websocket(*args, **kwargs):
-    channel_layer = channels.layers.get_channel_layer()
-    async_to_sync(channel_layer.group_send)("update", {
-        'type': 'pokemon.message',
-        'message': '{"type": "change", "model": "PokemonSpawn"}'
-    })
+    send_message('PokemonSpawn')
 
 
 def send_poi_update_to_websocket(*args, **kwargs):
-    channel_layer = channels.layers.get_channel_layer()
-    async_to_sync(channel_layer.group_send)("update", {
-        'type': 'pokemon.message',
-        'message': '{"type": "change", "model": "PointOfInterest"}'
-    })
+    send_message('PointOfInterest')
 
 
 def send_mapper_update_to_websocket(*args, **kwargs):
+    send_message('Mapper')
+
+
+def send_message(model_str):
     channel_layer = channels.layers.get_channel_layer()
     async_to_sync(channel_layer.group_send)("update", {
-        'type': 'pokemon.message',
-        'message': '{"type": "change", "model": "Mapper"}'
+        'type': 'update.message',
+        'updated':  timezone.now(),
+        'model_str': model_str
     })
 
 
--- volumes/app/myapp/websockets/consumers.py
+++ volumes/app/myapp/websockets/consumers.py
@@ -1,9 +1,16 @@
+from datetime import timedelta
+
 from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 
+from django.utils import timezone
+
 
 class PokemonConsumer(AsyncWebsocketConsumer):
+
     async def connect(self):
+        self.scope["session"]["last_updated"] = {}
+        self.scope["session"]['last_updated_default'] = timezone.now() - timedelta(minutes=20)
         await self.channel_layer.group_add(
             "update",
             self.channel_name
@@ -22,3 +29,17 @@ class PokemonConsumer(AsyncWebsocketConsumer):
 
     async def pokemon_message(self, event):
         await self.send(text_data=event["message"])
+
+    async def update_message(self, event):
+        updated = event['updated']
+        last_updated_dict = self.scope["session"]["last_updated"]
+        model_str = event['model_str']
+        message = {"type": "change", "model": model_str}
+        if model_str in last_updated_dict:
+            last_updated = last_updated_dict[model_str]
+        else:
+            last_updated = self.scope["session"]['last_updated_default']
+        if updated > last_updated + timedelta(seconds=5):
+            self.scope["session"]["last_updated"][model_str] = timezone.now()
+            await self.send(text_data=json.dumps(message))
+
