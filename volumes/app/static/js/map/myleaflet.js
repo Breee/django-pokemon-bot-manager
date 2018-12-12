a7e6015559f38d1af71f2d806aeb1a0f2525e3e4
Trigrab <trigrab@gmail.com>
2018-12-12 01:42:05 +0100

    fix: do not show image if reward unknown

--- volumes/app/static/js/map/myleaflet.js
+++ volumes/app/static/js/map/myleaflet.js
@@ -173,11 +173,16 @@ function setQuestPopup(poi_id) {
     if (quest.quest_pokemon_id === null || quest.quest_pokemon_id === undefined) {
         var reward = quest.quest_item_amount + 'x ' +
             '<img src="/static/img/quest_rewards/reward_' + quest.quest_item_id + '.png"' +
-            ' height="64px" width="64px" alt="item_' + quest.quest_item_id  + '">' + '<br>';
+            ' height="64px" width="64px" alt="item_' + quest.quest_item_id  + '">';
     } else {
-        var reward = '<img src="/static/img/pokemons/' + quest.quest_pokemon_id + '.png"' +
-            ' height="64px" width="64px" alt="item_' +
-            pokedex[quest.quest_pokemon_id].name_german  + '"><br>';
+        if (quest.quest_item_id !== null || quest.quest_item_id !== undefined) {
+            var reward = '<img src="/static/img/pokemons/' + quest.quest_pokemon_id + '.png"' +
+                ' height="64px" width="64px" alt="item_' +
+                pokedex[quest.quest_pokemon_id].name_german + '">'a;
+        }
+        else {
+            reward = ''
+        }
     }
     popup += 'Reward: ' + reward + '<br>';
     marker._popup.setContent(popup)
