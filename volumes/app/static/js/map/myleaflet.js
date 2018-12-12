01abb1f01594d38afee84ec1887b42191fc70afb
Trigrab <trigrab@gmail.com>
2018-12-12 02:08:11 +0100

    fix: predeceding zeros missing in front of img name

--- volumes/app/static/js/map/myleaflet.js
+++ volumes/app/static/js/map/myleaflet.js
@@ -171,8 +171,9 @@ function setQuestPopup(poi_id) {
     var quest = questDict[poi_id][2];
     popup += 'Quest: ' + quest.quest_template + '<br>';
     if (quest.quest_pokemon_id === null || quest.quest_pokemon_id === undefined) {
+        var quest_item_id_name = ("000" + quest.quest_item_id).slice(-4);
         var reward = quest.quest_item_amount + 'x ' +
-            '<img src="/static/img/Sprite/Item_' + quest.quest_item_id + '.png"' +
+            '<img src="/static/img/Sprite/Item_' + quest_item_id_name + '.png"' +
             ' height="64px" width="64px" alt="item_' + quest.quest_item_id  + '">';
     } else {
         if (quest.quest_item_id !== null || quest.quest_item_id !== undefined) {
