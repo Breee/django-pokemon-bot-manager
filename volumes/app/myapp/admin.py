9118ac27317b2cc3e67d5ef8224fa15fb1a91e63
Trigrab <trigrab@gmail.com>
2018-12-15 18:27:40 +0100

    add: raids to admin

--- volumes/app/myapp/admin.py
+++ volumes/app/myapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from myapp.models import PointOfInterest, Pokemon, PokemonSpawn, PokemonType, SpawnPoint, Quest, Mapper, \
-    AllowedDiscordServer
+from myapp.models import PointOfInterest, Pokemon, PokemonSpawn, PokemonType, SpawnPoint, Quest, \
+    Mapper, \
+    AllowedDiscordServer, Raid
 
 from rest_framework.authtoken.admin import TokenAdmin
 
@@ -13,6 +14,10 @@ class QuestAdmin(admin.ModelAdmin):
     search_fields = ['quest_id', 'pokestop_id', 'quest_pokemon_id', 'quest_item_id', 'quest_reward_type']
 
 
+class RaidAdmin(admin.ModelAdmin):
+    search_fields = ['poi_id', 'gym__name', 'pokemon_id__name_german', 'pokemon_id', 'level']
+
+
 # Register your models here.
 admin.site.register(Pokemon)
 admin.site.register(PokemonSpawn)
@@ -22,6 +27,7 @@ admin.site.register(Quest, QuestAdmin)
 admin.site.register(AllowedDiscordServer)
 admin.site.register(Mapper)
 admin.site.register(PointOfInterest, PointOfInterestAdmin)
+admin.site.register(Raid, RaidAdmin)
 
 
 TokenAdmin.raw_id_fields = ('user',)
