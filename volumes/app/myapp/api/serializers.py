1c47f30f223e7c0d6f15f3618d5d38d9b10d0493
Breee <loeffler.uni@gmail.com>
2019-01-06 23:14:37 +0100

    added custom raid field to PointOfInterestSerializer

--- volumes/app/myapp/api/serializers.py
+++ volumes/app/myapp/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from myapp.models import *
-
+import datetime
 
 class PokemonSerializer(serializers.ModelSerializer):
 
@@ -30,11 +30,35 @@ class PokemonSpawnSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+class RaidSerializer(serializers.ModelSerializer):
+    def __init__(self, *args, **kwargs):
+        many = kwargs.pop('many', True)
+        super(RaidSerializer, self).__init__(many=many, *args, **kwargs)
+
+    class Meta:
+        model = Raid
+        fields = '__all__'
+
+
 class PointOfInterestSerializer(serializers.ModelSerializer):
 
     def __init__(self, *args, **kwargs):
         many = kwargs.pop('many', True)
         super(PointOfInterestSerializer, self).__init__(many=many, *args, **kwargs)
+
+    # custom field to fetch raids for our points of interest.
+    raid = serializers.SerializerMethodField()
+
+    def get_raid(self, poi : PointOfInterest):
+        # Queries are expensive.
+        # We only want to check point of interests for raids, which are gyms and have been updated in the past 2 hours.
+        time_threshold = datetime.datetime.now(poi.last_updated.tzinfo) - datetime.timedelta(hours=2)
+        if poi.type == 'gym' and poi.last_updated > time_threshold:
+            raid = Raid.objects.filter(gym__poi_id=poi.poi_id).first()
+            # Serializing is expensive, only do it if there is a raid.
+            if raid:
+                return RaidSerializer(raid).data
+        return None
 
     class Meta:
         model = PointOfInterest
@@ -67,13 +91,6 @@ class PokeStopLureSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = PokeStopLure
-        fields = '__all__'
-
-
-class RaidSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Raid
         fields = '__all__'
 
 
